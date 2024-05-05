2559d5b831823ec3180ec9644d998bb5a3cbf0c3
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-05-05 11:56:47 -0300

    configuração form

---
+++ src/pages/form/Form.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Form() {
+    return (
+        <>
+            <form action="">
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" name="name" id="name" />
+                </div>
+            </form>
+        </>
+    )
+}
